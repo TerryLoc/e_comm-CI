c34dd5f755c7b5a9a8f9e8455b9df203e3645fe2
Terence Loughran <terryloughran@gmail.com>
2025-02-05 09:04:19 +0000

    add context.py for shopping bag

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",  # required for allauth
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "bag.contexts.bag_contents",
             ],
         },
     },
@@ -141,6 +142,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
